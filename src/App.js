dfa93ff8842c232db37f9216fda2e45eb7df076b
Mark Atherton <mark@insightinternet.co.uk>
2021-11-11 18:33:38 +0000

    end of lesson 192: deleted old fetchRequest function

--- src/App.js
+++ src/App.js
@@ -26,33 +26,6 @@ function App() {
     transformTasks
   );
 
-  const fetchTasks = async (taskText) => {
-    setIsLoading(true);
-    setError(null);
-    try {
-      const response = await fetch(
-        "https://react-http-4b88b-default-rtdb.europe-west1.firebasedatabase.app/tasks.json"
-      );
-
-      if (!response.ok) {
-        throw new Error("Request failed!");
-      }
-
-      const data = await response.json();
-
-      const loadedTasks = [];
-
-      for (const taskKey in data) {
-        loadedTasks.push({ id: taskKey, text: data[taskKey].text });
-      }
-
-      setTasks(loadedTasks);
-    } catch (err) {
-      setError(err.message || "Something went wrong!");
-    }
-    setIsLoading(false);
-  };
-
   useEffect(() => {
     fetchTasks();
   }, []);
